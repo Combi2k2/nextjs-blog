99b62a5ce94f5c87218f72b0871e4533c7e4020f
combi2k2 <cumbee2k2@gmail.com>
2025-03-04 14:49:16 +0700

    feat: on-demand revalidation

--- src/actions/actions.tsx
+++ src/actions/actions.tsx
@@ -2,14 +2,16 @@
 
 import { prisma } from "@/lib/prisma"
 import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
 export async function createBlog(formData: FormData) {
+    const title = formData.get("title") as string;
+    const slug = title.replace(/\s+/g, "-").toLowerCase();
+
     await prisma.blog.create({
         data: {
-            title: formData.get("title") as string,
-            slug: (formData.get("title") as string)
-                .replace(/\s+/g, "-")
-                .toLowerCase(),
+            title: title,
+            slug: slug,
             content: formData.get("content") as string,
             excerpt: formData.get("summary") as string,
             tags: (formData.get("tags") as string)
@@ -18,5 +20,44 @@ export async function createBlog(formData: FormData) {
                 .filter((tag: string) => tag !== '')
         }
     });
+
+    revalidatePath("/blogs");
+    revalidatePath(`/blogs/${slug}`);
+    redirect("/blogs");
+}
+
+export async function updateBlog(id: string, formData: FormData) {
+    const title = formData.get("title") as string;
+    const slug = title.replace(/\s+/g, "-").toLowerCase();
+
+    await prisma.blog.update({
+        where: {
+            id: id,
+        },
+        data: {
+            title: title,
+            slug: slug,
+            content: formData.get("content") as string,
+            excerpt: formData.get("summary") as string,
+            tags: (formData.get("tags") as string)
+                .split(',')
+                .map((tag: string) => tag.trim())
+                .filter((tag: string) => tag !== '')
+        }
+    });
+
+    revalidatePath("/blogs");
+    revalidatePath(`/blogs/${slug}`);
+    redirect("/blogs");
+}
+
+export async function deleteBlog(id: string) {
+    await prisma.blog.delete({
+        where: {
+            id: id,
+        },
+    });
+
+    revalidatePath("/blogs");
     redirect("/blogs");
 }
